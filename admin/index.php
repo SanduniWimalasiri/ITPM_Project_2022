77c2c53d3b814b4839f8d011218997ec4eeca4f3
ashanmillewa <ashanmillewa@gmail.com>
2022-05-05 17:20:04 +0530

    footer

---
+++ admin/index.php
@@ -0,0 +1,86 @@
+<html>
+    <head>
+        <title> Electrnic Item Store</title>
+        <link rel="stylesheet" href="../css/admin.css">
+    </head>
+
+
+    <body>
+
+        <!-- menu section start-->
+        <div class="menu">
+            <div class="wrapper">
+            menu goes here
+            </div>
+        </div>
+        <!-- menu section end-->
+
+        <!-- main content section start-->
+        <div class="main-content">
+            <div class="wrapper">
+            main content goes here
+            </div>
+        </div>
+        <!-- main content section end-->
+
+   
+        <!--Footer Section Start -->
+<div class="footer">
+        <div class="wrapper">
+
+           <table border ="0" width="100%" height="100px">
+                <tr>
+                
+            <!-- contact Section Starts Here -->
+                <td width = "33.33%" class ="text-left">
+                    <p> <h4>Hotline : 01123456789 <br>Email : electronicstore@gmail.com</h4></p>
+                </td>
+            <!-- contact Section Ends Here -->
+
+                
+            <!-- address Section Starts Here -->
+            <td width = "33.33%" class ="text-justify">
+                    <p> <h4>No : 32/32, <br>Main Street,<br> Colombo.</h4></p>
+                    <p>©All rights reserved.</p>
+                </td>
+            <!-- address Section Ends Here -->
+                    
+                    
+                
+            <!-- social Section Starts Here -->
+            <td width = "33.33%" class ="text-center">
+                <section class="social">
+                    <div class="container text-center">
+                        <ul width = "10%" >
+                            <li>
+                                <a href="https://www.facebook.com/"><img src="https://img.icons8.com/fluent/50/000000/facebook-new.png"/></a>
+                            </li>
+                            <li>
+                                <a href="https://www.instagram.com/"><img src="https://img.icons8.com/fluent/48/000000/instagram-new.png"/></a>
+                            </li>
+                            <li>
+                                <a href="https://twitter.com/"><img src="https://img.icons8.com/fluent/48/000000/twitter.png"/></a>
+                            </li>
+                        </ul>
+                    </div>
+                </section>
+                <!-- social Section Ends Here -->
+ 
+                </td></tr>
+            </table>
+
+            </div>
+        </div>
+
+
+    <!--Footer Section End -->
+
+  
+
+
+
+
+
+    </body>
+
+</html>
